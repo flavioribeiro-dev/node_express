67762acd4c5353782ff94204b3880eb2626f9399
fribeiro <flavioxconsult@gmail.com>
2026-06-01 13:50:15 -0300

    rota GET excluido atributos do Objeto retornado

--- 1. api/src/main.ts
+++ 1. api/src/main.ts
@@ -3,32 +3,52 @@ import { configDotenv } from 'dotenv';
 import filmes from './dados/filmes.ts';
 
 configDotenv();
-// const porta = Number(process.env.PORT);
 
 const app = express();
 
+// Rota de Teste inicial
 app.get('/ping', (req, res) => {
     res.send('pong');
 })
 
+// Consultar TODOS os Filmes
+// app.get('/filmes', (req, res) => {
+//     return res.status(200).json(filmes);
+// })
+
+function limparCampos(ignorar:string | undefined, filmes:any) {
+    const camposPraIgnorar = ignorar ? ignorar.toString().split(',') : [];
+    const copia:any = {...filmes};
+    camposPraIgnorar.forEach( (campo:string) => {delete copia[campo]} );
+    return copia;
+}
+
 app.get('/filmes', (req, res) => {
-    return res.status(200).json(filmes);
+    const { ignorar } = req.query as any;
+    const filmesProcessados = filmes.map( (filme:any) => limparCampos(ignorar, filme) )
+    return res.status(200).json(filmesProcessados);
 })
 
 
-app.get('/filmes/oi', (req, res) => {
+// Teste a ordem de precedência das rotas - um parâmetro "estático" sempre precisa vir antes de um parâmetro dinâmico (comparando filmes/oi com filmes/:id - este último precisa vir após o primeiro)
+app.get('/filmes/oi', (req, res) => { 
     res.json('oi')
 })
 
+
+// Consultando filme a partir do ID e obtendo Parâmetros a partir da própria requisição
 app.get('/filmes/:id', (req, res) => {
     const { id } = req.params;
-    const filme = filmes.find((f:any)=>f.id === id);
+    const { ignorar } = req.query as any;
+    const filme = filmes.find((f:any) => f.id === id);
     if(!filme) {
-        res.status(404).send('Filme não disponível');
-        return
+        res.status(404).send('Filme não encontrado');
+        return;
     }
-    return res.status(200).json({ filme })
+    return res.status(200).json( limparCampos(ignorar, filme) );
 })
+
+
 app.listen(process.env.PORT, () => {
     console.log(`servidor rodando na porta ${process.env.PORT}`)
 })
